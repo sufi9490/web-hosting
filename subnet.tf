a9fcd40a1142474ba5a171541a147d839020a096
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-22 18:18:25 +0700

    commit

---
+++ subnet.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.2.0.0/16"
+}
+
+resource "aws_subnet" "in_secondary_cidr" {
+  vpc_id     = vpc-0b71ecdc3bd350580
+  cidr_block = "172.2.0.0/24"
+}
