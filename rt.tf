f61e3a2df4db8e6370ea07b53a0ad82c19270c33
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 23:38:31 +0700

    commit all files

---
+++ rt.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table" "table" {
+  vpc_id = "${aws_vpc.main.id}"
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+  tags = {
+    Name = "MyRoute"
+    Purpose = "Demo_Project"
+  }
+}
