34ac0d51d4fc68b14615a87a1a1041d904230980
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 22:16:33 +0700

    add

--- main.tf
+++ main.tf
@@ -7,15 +7,6 @@ resource "aws_s3_bucket" "b1" {
 
   acl    = "public-read"
 
-  policy = file("policy.json")
-
-  website {
-
-    index_document = "index.html"
-
-    error_document = "error.html"
-  }
-
 
   tags = {
 
