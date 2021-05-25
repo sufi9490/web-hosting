5fbc6490b1b228479fd747ac792d629c392608ca
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 14:12:14 +0700

    commit this

---
+++ s3-website/s3.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "static_site" {
+    bucket = "lonlywolf24"
+    acl = "public-read-write"
+    
+    tags = {
+    	Name = "demo_s3"
+        Purpose = "Demo_Project_Presentation"
+    }
+ website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "index.html"
+  source       = "html/index.html"
+  content_type = "text/html"
+  etag         = "${md5(file("html/index.html"))}"
+  acl          = "public-read"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "error.html"
+  source       = "html/error.html"
+  content_type = "text/html"
+  etag         = "${md5(file("html/error.html"))}"
+  acl          = "public-read"
+}
