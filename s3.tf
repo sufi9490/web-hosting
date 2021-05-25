8a0bcc74025773bfa938265c91162fbaf0a97100
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 13:45:27 +0700

    commit the changs for static website

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "lonlywolf22" {
+resource "aws_s3_bucket" "static_site" {
     bucket = "lonlywolfks"
     acl = "public-read-write"
     
@@ -6,4 +6,26 @@ resource "aws_s3_bucket" "lonlywolf22" {
     	Name = "demo_s3"
         Purpose = "Demo_Project_Presentation"
     }
+ website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "index.html"
+  source       = "html/index.html"
+  content_type = "text/html"
+  etag         = "${md5(file("html/index.html"))}"
+  acl          = "public-read"
+}
+
+resource "aws_s3_bucket_object" "error" {
+  bucket       = "${aws_s3_bucket.static_site.bucket}"
+  key          = "error.html"
+  source       = "html/error.html"
+  content_type = "text/html"
+  etag         = "${md5(file("html/error.html"))}"
+  acl          = "public-read"
 }
