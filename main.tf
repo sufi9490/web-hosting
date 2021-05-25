7d14712785b6977bd455ab66571e1c2baf681c51
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 20:54:50 +0700

    add

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+provider "aws"{
   region = "us-east-2"
 }
 
@@ -7,8 +7,6 @@ resource "aws_s3_bucket" "site" {
   bucket = "s3-terraform-bucket-lab-demo"
 
   acl    = "public-read"
-
-  policy = aws_s3_bucket.site
 
   website {
 
@@ -35,24 +33,6 @@ resource "aws_s3_bucket" "b1" {
 
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-  bucket = aws_s3_bucket.site.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          aws_s3_bucket.site.arn,
-          "${aws_s3_bucket.site.arn}/*",
-        ]
-      },
-    ]
-  })
-}
 
 resource "aws_s3_bucket_object" "object1" {
 
