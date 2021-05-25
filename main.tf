aeeae3a0136dfc3f8d29b10ffa64dcecde360004
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 22:12:09 +0700

    add

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ resource "aws_s3_bucket" "b1" {
 
   policy = file("policy.json")
 
+  website {
+
+    index_document = "index.html"
+
+    error_document = "error.html"
+  }
+
+
   tags = {
 
     Name        = "My bucket"
@@ -46,19 +54,3 @@ resource "aws_s3_bucket_object" "object2" {
   etag = filemd5("html/error.html")
 
 }
-
-resource "aws_s3_bucket" "website" {
-
-  bucket = "s3-terraform-bucket-lab-demo"
-
-  acl    = "public-read"
-
-  policy = file("policy.json")
-
-  website {
-
-    index_document = "index.html"
-
-    error_document = "error.html"
-  }
-
