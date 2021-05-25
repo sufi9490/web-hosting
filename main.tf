50ee44a1b90017f739bd34a06d9ed214acc66583
PrekshaSbabu <malupreksha98@gmail.com>
2021-05-25 13:37:23 +0700

    main file for wesite creation

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {}
+
+module "s3-website" {
+  source = "../../modules/s3-website"
+
+  bucket_name = "lonlywolfks"
+}
+
+output "website_endpoint" {
+  value = module.s3-website.website_endpoint
+}
+
